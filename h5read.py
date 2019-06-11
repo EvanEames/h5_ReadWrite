c1a476c3d98704b6f5da5b445ea3b395f5099da3
Evan <38322201+EvanEames@users.noreply.github.com>
2019-06-11 11:36:18 +0200

    Update h5read.py

--- h5read.py
+++ h5read.py
@@ -9,6 +9,7 @@ train_dataset = h5py.File('datasets/train_cars.h5', "r")
 #And break it up into the features and labels:
 train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
 train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+#It's good to verify this is true as a quick sanity check
 
 #Also, assuming this is image data created with the accopanying h5_write.py file, the resulting variables should have the shape:
 #np.shape(train_set_x_orig) = (#Images,ImageHeight,ImageWidth,Channels)
